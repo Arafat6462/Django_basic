0f19455b3a3585b9d71fc4b5c910ba53afbaf6c6
Arafat Hossain <arafat6462@gmail.com>
2025-07-09 15:02:58 +0600

    Add post list and detail views to the blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Comment
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all().order_by('-pub_date') # Get all posts, ordered by publication date
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk) # Get a single post or return a 404 error
+    comments = post.comments.all() # Get all comments related to this post
+    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments})
