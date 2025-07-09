c62935ea5929843e0e825e76b5041a3eb6f3655b
Arafat Hossain <arafat6462@gmail.com>
2025-07-09 17:36:37 +0600

    Add base, index, and detail templates for blog app

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,9 @@ from .models import Post, Comment
 
 def post_list(request):
     posts = Post.objects.all().order_by('-pub_date') # Get all posts, ordered by publication date
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/index.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk) # Get a single post or return a 404 error
     comments = post.comments.all() # Get all comments related to this post
-    return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments})
+    return render(request, 'blog/detail.html', {'post': post, 'comments': comments})
