7c472c64aa19147aa54176a0ea1551f4838c1453
Arafat Hossain <arafat6462@gmail.com>
2025-07-09 23:06:08 +0600

    Add author_user field to Comment model and update related migrations

--- blog/models.py
+++ blog/models.py
@@ -1,20 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User # Import Django's built-in User model
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True) # Automatically sets the date when the object is first created
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # Link to Django's User model
 
     def __str__(self):
         return self.title
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments') # Link to the Post model
+    author = models.CharField(max_length=100)
+    author_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True) # Temporary field
     text = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True) # Automatically sets the date when the object is first created
 
     def __str__(self):
-        return f"Comment by {self.author.username} on {self.post.title}"
+        return f"Comment by {self.author} on {self.post.title}"
