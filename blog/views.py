46daaa69adde9f88be47caea87e7c8ad0b6e7042
Arafat Hossain <arafat6462@gmail.com>
2025-07-09 19:27:02 +0600

    Implement comment functionality in post detail view

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
+from .forms import CommentForm
 
 def post_list(request):
     posts = Post.objects.all().order_by('-pub_date') # Get all posts, ordered by publication date
     return render(request, 'blog/index.html', {'posts': posts})
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk) # Get a single post or return a 404 error
-    comments = post.comments.all() # Get all comments related to this post
-    return render(request, 'blog/detail.html', {'post': post, 'comments': comments})
+    post = get_object_or_404(Post, pk=pk)
+    comments = post.comments.all()
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        pass
+        form = CommentForm()
+
+    return render(request, 'blog/detail.html', {'post': post, 'comments': comments, 'form': form})
