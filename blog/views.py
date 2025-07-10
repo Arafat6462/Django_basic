9cf39df40067034b8f5cacc38e54515df2b38bbf
Arafat Hossain <arafat6462@gmail.com>
2025-07-10 06:53:13 +0600

    Refactor comment author handling: remove legacy author field and rename author_user to author; update related migrations and views for comment submission.

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,32 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Post, Comment
 from .forms import CommentForm
 
 def post_list(request):
-    posts = Post.objects.all().order_by('-pub_date') # Get all posts, ordered by publication date
+    posts = Post.objects.all().order_by('-pub_date')
     return render(request, 'blog/index.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     comments = post.comments.all()
+    form = CommentForm()
 
+    return render(request, 'blog/detail.html', {
+        'post': post,
+        'comments': comments,
+        'form': form,
+    })
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.post = post
+            comment.author = request.user
             comment.save()
             return redirect('post_detail', pk=post.pk)
-    else:
-        pass
-        form = CommentForm()
-
-    return render(request, 'blog/detail.html', {'post': post, 'comments': comments, 'form': form})
+    return redirect('post_detail', pk=post.pk) # Redirect if not a POST request
